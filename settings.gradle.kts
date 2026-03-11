f304ab6eab5d6c8da16965b8c01b8ad0f562128c
mikhail <58307835+savin000@users.noreply.github.com>
2026-03-12 00:30:42 +0300

    update repositories configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,3 +6,10 @@ pluginManagement {
         mavenCentral()
     }
 }
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        maven("https://packages.confluent.io/maven/")
+    }
+}
